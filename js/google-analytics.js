7c5a9a92df1080cd4901f9af21095f0e1df2fab8
Summer <1143473794@qq.com>
2024-05-10 15:45:38 +0800

    Site updated: 2024-05-10 15:45:37

---
+++ js/google-analytics.js
@@ -0,0 +1,17 @@
+
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-WNRXFL2L0N"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-WNRXFL2L0N');
+// </script>
+
+
+// src="https://www.googletagmanager.com/gtag/js?id=G-WNRXFL2L0N"
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-WNRXFL2L0N');
